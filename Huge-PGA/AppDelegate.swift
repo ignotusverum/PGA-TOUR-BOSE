c8052753b4dbea879e3764bfe523d4a1b234e2b4
Vlad Z <vladislavz1991@gmail.com>
2020-02-11 10:53:20 -0500

    init

---
+++ Huge-PGA/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Huge-PGA
+//
+//  Created by Vlad Z. on 2/11/20.
+//  Copyright © 2020 Vlad Z. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,20 @@
+platform :ios, '11.0'
+use_frameworks!
+use_modular_headers!
+inhibit_all_warnings! # supresses pods project warnings
+
+def common
+  pod 'MERLin'
+  pod 'RxDataSources'
+  pod 'SwiftGen', '~> 5.3.0'
+end
+
+target 'Huge-PGA' do
+  common  
+end
+
+abstract_target 'Modules' do
+  common
+  
+end
+
